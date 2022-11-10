a86cc7e85140c5a1595a225719b06271652ced6f
greatSeo <sjseo85@gmail.com>
2022-11-10 22:13:45 +0900

    숨어있던 코드들 발견

---
+++ boj/1008.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+
+A, B = map(float, input().split())
+
+print(A/B)
---
+++ boj/15650.py
@@ -0,0 +1,33 @@
+import sys
+
+def nm_combination_without_duple(N:int, M:int):
+    def sub_function(num:int):
+        if num == M:
+            temp_apend = rslt_val.copy()
+            rtn_val.append(temp_apend)
+            return
+        
+        for i in range(1, N+1):
+            if visited[i] == False:
+                visited[i] = True
+                rslt_val.append(i)
+                sub_function(num+1)
+                #visited[i] = False
+                rslt_val.pop()
+    
+    rslt_val = []
+    rtn_val = []
+    visited = [False] * (N+1)
+    sub_function(0)
+    
+    return rtn_val
+            
+
+if __name__ == '__main__':
+    input = sys.stdin.readline
+    N, M= map(int, input().split(' '))
+    
+    temp = nm_combination_without_duple(N, M)
+    temp.sort()
+    for i in temp:
+        print(' '.join(map(str, i)))
